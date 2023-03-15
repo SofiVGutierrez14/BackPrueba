05da402c002979c63a7f80be81bce87e0c0bb2c5
SofiVGutierrez14 <sofi.gutierrez@live.com>
2023-03-15 18:22:50 -0300

    pruebo con otra url

--- ArgentinaPrograma/src/main/java/com/example/ArgentinaPrograma/controller/ControllerPersona.java
+++ ArgentinaPrograma/src/main/java/com/example/ArgentinaPrograma/controller/ControllerPersona.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "https://hosting-angular-49dee.web.app")
+@CrossOrigin(origins = "https://backprueba-production-7b63.up.railway.app")
 public class ControllerPersona {
     
   @Autowired
